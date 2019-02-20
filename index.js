3f0a8c1b3a6474cec3b709f3ffe78728d007266d
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-02-20 01:11:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,10 +24,13 @@ function longPlaneteerCalls(words) {
 }}
 
 function findTheCheese (foods) {
-  for (var i = 0; i < foods.length; i++) {
-    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
-      return foods[i];
-    }
-      return "no cheese!";
+  for (var i = 0; i<foods.length; i++)
+   {
+     if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert")
+     {
+       return foods[i];
+     }
+   }
+   return "no cheese!";
 }
 }
